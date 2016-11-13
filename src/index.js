31d3672da47ba9458540ded680998335c454d61a
Florent Berthelot <FBerthelot@users.noreply.github.com>
2016-11-13 19:09:52 +0100

    Add a button to select dynamicaly the audio source

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
-angular.module('distortion', [])
+'use strict';
 
-angular.module('distortion').controller('DistortionController', DistortionController)
+angular.module('distortion', []);
+
+angular.module('distortion').controller('DistortionController', DistortionController);
 
 function DistortionController () {
+  var vm = this;
 
   //Config
-  equalizerFrequencies = [50, 100, 200, 400, 800, 1500, 3000, 5000, 7000, 10000, 15000] 
+  var equalizerFrequencies = [50, 100, 200, 400, 800, 1500, 3000, 5000, 7000, 10000, 15000];
 
-  var vm = this
-  vm.gain = 50
-  vm.distortion = 0
-  vm.equalizer = {}
-  //Creation de l'audio contexte
-  var audioContext = new AudioContext()
+  vm.sources = [
+    {name: 'Guitare acoustique 1', url: './assets/acoustic.wav'},
+    {name: 'Guitare acoustique 2', url: './assets/acoustic2.wav'},
+    {name: 'Guitare acoustique 5', url: './assets/acoustic5.wav'},
+    {name: 'Violon', url: './assets/violin.wav'}
+  ];
 
-  //Creation de la source audio
-  var audio = new Audio('./assets/acoustic.wav')
-  audio.loop = true
+  vm.gain = 50;
+  vm.distortion = 0;
+  vm.equalizer = {};
+  //Creation du contexte audio
+  var audioContext = new AudioContext();
 
   //Creation du noeud de gain
-  var gainNode = audioContext.createGain()
-  gainNode.gain.value = vm.gain / 100
+  var gainNode = audioContext.createGain();
+  gainNode.gain.value = vm.gain / 100;
 
   vm.changeGain = function(value) {
-    gainNode.gain.value = value / 100
-  }
+    gainNode.gain.value = value / 100;
+  };
 
   //Creation du noeud de distortion
-  var distortionNode = audioContext.createWaveShaper()
+  var distortionNode = audioContext.createWaveShaper();
   function makeDistortionCurve(amount) {
     var k = typeof amount === 'number' ? amount : 50,
       n_samples = 44100,
@@ -46,41 +51,49 @@ function DistortionController () {
 
   vm.changeDistortion = function(value) {
     distortionNode.curve = makeDistortionCurve(parseInt(value));
-  }
+  };
 
   //Creation des noeud de l'equalizer'
   var equalizerNodes = equalizerFrequencies.map(function(frequency) {
-    var filterNode = audioContext.createBiquadFilter()
+    var filterNode = audioContext.createBiquadFilter();
     filterNode.type = "peaking";
-    filterNode.frequency.value = frequency
-    filterNode.gain.value = 0
+    filterNode.frequency.value = frequency;
+    filterNode.gain.value = 0;
     vm.equalizer[frequency] = {
       node : filterNode,
       value: 0
-    }
-    return filterNode
-  })
+    };
+    return filterNode;
+  });
 
-  vm.changeEqualizer = function(value, node){
-    console.log('frequency', node.frequency.value, 'set to', value)
-    node.gain.value = value
+  vm.changeEqualizer = function(value, node) {
+    console.log('frequency', node.frequency.value, 'set to', value);
+    node.gain.value = value;
   }
 
-  /* 
-    Branchement des nodes : 
+  /*
+    Branchement des nodes :
     [source] => [gain] => [output]
   */
-  var source = audioContext.createMediaElementSource(audio)
+
   var lastEqualizerNode = equalizerNodes.reduce(function(previousNode, frequency) {
-    console.log(frequency)
-    previousNode.connect(frequency)
-    return frequency
-  }, source)
-  lastEqualizerNode.connect(distortionNode)
-  distortionNode.connect(gainNode)
-  gainNode.connect(audioContext.destination)
-  audio.play()
+    previousNode && previousNode.connect(frequency);
+    return frequency;
+  }, null);
+  lastEqualizerNode.connect(distortionNode);
+  distortionNode.connect(gainNode);
+  gainNode.connect(audioContext.destination);
 
-  
+  //Creation de la source audio
+  vm.changeSource = function(source) {
+    if(vm.audioSource) {
+      vm.sourceNode.disconnect(0);
+    }
+    vm.audioSource = new Audio(source);
+    vm.audioSource.loop = true;
+    vm.audioSource.play();
+
+    vm.sourceNode = audioContext.createMediaElementSource(vm.audioSource);
+    vm.sourceNode.connect(equalizerNodes[0]);
+  };
 }
-
