3ecee15dea8c162cf224218c7ab0de1f80634776
Okazari <plouzennec.benjamin@gmail.com>
2017-05-01 16:35:47 +0200

    Step4 Distorsion

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import source from './source'
 import gain from './gain'
 import spectre from './spectre'
 import osciloscope from './osciloscope'
+import distorsion from './distorsion'
 
 source.connect(gain)
-gain.connect(osciloscope)
+gain.connect(distorsion)
+distorsion.connect(osciloscope)
 osciloscope.connect(spectre)
 spectre.connect(audioContext.destination)
