680918d9d96d05890e1a5faf1849e1b1bb15ff48
FBerthelot <florent.berthelot35@gmail.com>
2017-05-06 21:50:50 +0200

    Add gain node

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import audioContext from './audioContext'
 import source from './source'
+import gain from './gain';
 
-source.connect(audioContext.destination)
+source.connect(gain)
+gain.connect(audioContext.destination)
