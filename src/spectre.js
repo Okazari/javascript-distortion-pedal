0e52056f2a058cd54aa1a402fd851c5646d5b7dd
FBerthelot <florent.berthelot35@gmail.com>
2017-05-06 21:54:34 +0200

    Add spectre

--- src/spectre.js
+++ src/spectre.js
@@ -3,17 +3,26 @@ import audioContext from './audioContext'
 const spectre = document.getElementById('spectre')
 const spectreCtx = spectre.getContext('2d')
 
+const analyserSpectre = audioContext.createAnalyser()
+analyserSpectre.fftSize = 256
+analyserSpectre.smoothingTimeConstant = 0.9;
+
+const bufferSpectreLength = analyserSpectre.frequencyBinCount
+const dataSpectreArray = new Uint8Array(bufferSpectreLength)
+
 const drawSpectre = () => {
   requestAnimationFrame(drawSpectre)
+
+  analyserSpectre.getByteFrequencyData(dataSpectreArray)
 
   spectreCtx.fillStyle = 'rgb(0, 0, 0)'
   spectreCtx.fillRect(0, 0, spectre.width, spectre.height)
 
-  const lBarre = Math.floor((spectre.width / 150) * 2.5)
+  const lBarre = Math.floor((spectre.width / bufferSpectreLength) * 2.5)
 
   let x = 0
-  for(let i = 0; i < 150; i++) {
-    const hBarre = 50
+  for(let i = 0; i < bufferSpectreLength; i++) {
+    let hBarre = dataSpectreArray[i]
 
     spectreCtx.fillStyle = '#ABDCF6'
     spectreCtx.fillRect(x, spectre.height - hBarre / 1.5, lBarre, hBarre)
