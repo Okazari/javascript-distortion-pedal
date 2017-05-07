63611b80449da571777202c67897fc9d7df6a2cf
FBerthelot <florent.berthelot35@gmail.com>
2017-05-07 16:09:19 +0200

    add osciloscope

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import audioContext from './audioContext'
 import source from './source'
 import gain from './gain'
 import spectre from './spectre'
+import osciloscope from './osciloscope'
 
 source.connect(gain)
 gain.connect(spectre)
-spectre.connect(audioContext.destination)
+spectre.connect(osciloscope)
+osciloscope.connect(audioContext.destination)
--- src/osciloscope.js
+++ src/osciloscope.js
@@ -1,25 +1,37 @@
 import audioContext from './audioContext'
 
+const analyserOsciloscope = audioContext.createAnalyser();
+analyserOsciloscope.fftSize = 2048;
+analyserOsciloscope.maxDecibels = 10;
+analyserOsciloscope.minDecibels = 0;
+
 const osciloscope = document.getElementById('oscilloscope');
 const osciloCtx = osciloscope.getContext('2d');
+const bufferLength = analyserOsciloscope.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
 
 function drawOsciloscope() {
-    requestAnimationFrame(drawOsciloscope);
-    osciloCtx.fillStyle = 'rgb(0, 0, 0)';
-    osciloCtx.fillRect(0, 0, osciloscope.width, osciloscope.height);
-    osciloCtx.lineWidth = 2;
-    osciloCtx.strokeStyle = '#ABDCF6';
+  requestAnimationFrame(drawOsciloscope);
+  osciloCtx.fillStyle = 'rgb(0, 0, 0)';
+  osciloCtx.fillRect(0, 0, osciloscope.width, osciloscope.height);
+  osciloCtx.lineWidth = 2;
+  osciloCtx.strokeStyle = '#ABDCF6';
 
+  analyserOsciloscope.getByteTimeDomainData(dataArray);
 
-    osciloCtx.beginPath();
-    let x = 0;
-    let y = osciloscope.height / 2;
-    osciloCtx.moveTo(x, y);
+  osciloCtx.beginPath();
 
-    x = osciloscope.width;
-    osciloCtx.lineTo(x, y);
+  const sliceWidth = osciloscope.width * 1.0 / bufferLength;
+  let x = 0;
+  for(let i = 0; i < bufferLength; i++) {
+    const y = (dataArray[i] / 128.0) * (osciloscope.height / 2);
 
-    osciloCtx.stroke();
+    if(i === 0) { osciloCtx.moveTo(x, y); }
+    else { osciloCtx.lineTo(x, y); }
+
+    x += sliceWidth;
+  }
+  osciloCtx.stroke();
 }
 drawOsciloscope();
 
