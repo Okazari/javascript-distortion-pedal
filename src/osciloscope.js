c222c4f2624ea304e86c2969ada0926abc61d813
FBerthelot <florent.berthelot35@gmail.com>
2017-05-07 16:09:19 +0200

    add osciloscope

--- src/osciloscope.js
+++ src/osciloscope.js
@@ -5,35 +5,32 @@ analyserOsciloscope.fftSize = 2048;
 analyserOsciloscope.maxDecibels = 10;
 analyserOsciloscope.minDecibels = 0;
 
+const osciloscope = document.getElementById('oscilloscope');
+const osciloCtx = osciloscope.getContext('2d');
 const bufferLength = analyserOsciloscope.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
 
-const osciloscope = document.getElementById('oscilloscope');
-const osciloCtx = osciloscope.getContext('2d');
-
 function drawOsciloscope() {
-    requestAnimationFrame(drawOsciloscope);
-    osciloCtx.fillStyle = 'rgb(0, 0, 0)';
-    osciloCtx.fillRect(0, 0, osciloscope.width, osciloscope.height);
-    osciloCtx.lineWidth = 2;
-    osciloCtx.strokeStyle = '#ABDCF6';
+  requestAnimationFrame(drawOsciloscope);
+  osciloCtx.fillStyle = 'rgb(0, 0, 0)';
+  osciloCtx.fillRect(0, 0, osciloscope.width, osciloscope.height);
+  osciloCtx.lineWidth = 2;
+  osciloCtx.strokeStyle = '#ABDCF6';
 
-    analyserOsciloscope.getByteTimeDomainData(dataArray);
+  analyserOsciloscope.getByteTimeDomainData(dataArray);
 
-    osciloCtx.beginPath();
+  osciloCtx.beginPath();
 
-    const sliceWidth = osciloscope.width * 1.0 / bufferLength;
-    let x = 0;
-    for(let i = 0; i < bufferLength; i++) {
-      const y = (dataArray[i] / 128.0) * (osciloscope.height / 2);
+  const sliceWidth = osciloscope.width * 1.0 / bufferLength;
+  let x = 0;
+  for(let i = 0; i < bufferLength; i++) {
+    const y = (dataArray[i] / 128.0) * (osciloscope.height / 2);
 
-      if(i === 0) { osciloCtx.moveTo(x, y); }
-      else { osciloCtx.lineTo(x, y); }
-
-      x += sliceWidth;
-    }
-
-    osciloCtx.stroke();
+    if(i === 0) { osciloCtx.moveTo(x, y); }
+    else { osciloCtx.lineTo(x, y); }
+    x += sliceWidth;
+  }
+  osciloCtx.stroke();
 }
 drawOsciloscope();
 
