f52b3ebfd30134c8f2acf45577fb3bb9d2999daa
Okazari <plouzennec.benjamin@gmail.com>
2017-05-11 13:20:13 +0200

    Url!!!

--- src/source.js
+++ src/source.js
@@ -2,6 +2,7 @@ import audioContext from './audioContext'
 import { connectPlayButton } from './utils'
 
 const url = '/assets/acoustic.wav'
+
 const audio = new Audio(url)
 audio.loop = true
 connectPlayButton(audio)
